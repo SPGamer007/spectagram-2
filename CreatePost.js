cf5e514362c500bd7f2a664d6d01e9ad94cac98a
SPGamer007 <129699948+SPGamer007@users.noreply.github.com>
2024-07-08 18:55:07 +1000

    Add files via upload

---
+++ CreatePost.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
+
+export default class CreatePost extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>CreatePost</Text>
+            </View>
+        )
+}
+}
---
+++ Profile.js
@@ -0,0 +1,23 @@
+import React, { Component } from "react";
+import {
+    View,
+    Text,
+    StyleSheet,
+    SafeAreaView,
+    TouchableOpacity,
+    Platform,
+    StatusBar,
+    ImageBackground,
+    Image
+} from "react-native";
+
+export default class Profile extends Component {
+
+    render() {
+        return (
+            <View>
+                <Text>Profile</Text>
+            </View>
+        )
+}
+}
